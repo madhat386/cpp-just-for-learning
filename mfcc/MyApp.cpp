173ac9f301771ed66d3d5d77796ea6490f1bf20a
madhat386 <307262079@qq.com>
2021-02-24 03:10:38 +0800

    update

--- mfcc/MyApp.cpp
+++ mfcc/MyApp.cpp
@@ -14,6 +14,8 @@ BOOL MyApp::InitInstance() {
 //消息映射的分界宏
 BEGIN_MESSAGE_MAP(MyFrame, CFrameWnd)//参数：消息映射的窗口类和其父类
 	ON_WM_LBUTTONDOWN() //鼠标左键按下的宏
+	ON_WM_CHAR() //键盘按下的宏
+	ON_WM_PAINT() //绘图的宏
 END_MESSAGE_MAP()
 
 
@@ -23,8 +25,26 @@ MyFrame::MyFrame() {
 
 //鼠标左键按下的实现
 void MyFrame::OnLButtonDown(UINT nFlags, CPoint point) {
-	TCHAR buf[1024];
-	wsprintf(buf, TEXT("x=%d,y=%d"), point.x, point.y);
-	MessageBox(buf);
+	//TCHAR buf[1024];
+	//wsprintf(buf, TEXT("x=%d,y=%d"), point.x, point.y);
+	//MessageBox(buf);
+	CString str;//MFC中使用的字符串
+	//格式化字符串
+	str.Format(TEXT("x=%d,y=%d"), point.x, point.y);
+	MessageBox(str);
 
+}
+
+//按键按下且抬起前触发的方法实现
+void MyFrame::OnChar(UINT key, UINT, UINT) {
+	CString str;
+	str.Format(TEXT("按下了%C键"), key);
+	MessageBox(str);
+}
+
+//绘图事件的实现
+void MyFrame::OnPaint() {
+	CPaintDC dc(this);//dc相当于画家，this是绘制的所在窗口
+	dc.TextOutW(100, 100, TEXT("Hello world"));//绘制文字
+	dc.Ellipse(10, 10, 100, 100);//根据矩形（对角线的两个点）绘制内切圆
 }
--- mfcc/MyApp.h
+++ mfcc/MyApp.h
@@ -17,6 +17,8 @@ public:
 	DECLARE_MESSAGE_MAP()//声明宏
 
 	afx_msg void OnLButtonDown(UINT, CPoint);//鼠标左键按下处理方法声明
+	afx_msg void OnChar(UINT, UINT, UINT);//按键按下且抬起前触发的方法声明
+	afx_msg void OnPaint();//绘图事件的声明
 
 };
 
