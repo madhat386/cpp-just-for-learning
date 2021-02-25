78301495b613a5b3a8225380c314d7d01bbf33b0
madhat386 <307262079@qq.com>
2021-02-26 01:16:08 +0800

    create

---
+++ MFCTabCtrl/MFCTabCtrlDlg.h
@@ -0,0 +1,36 @@
+﻿
+// MFCTabCtrlDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CMFCTabCtrlDlg 对话框
+class CMFCTabCtrlDlg : public CDialogEx
+{
+// 构造
+public:
+	CMFCTabCtrlDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCTABCTRL_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+private:
+	CTabCtrl m_tab;
+};
