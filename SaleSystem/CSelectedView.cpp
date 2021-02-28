5d2d8a80650fcbbecd8b27f186a78226a0996a52
madhat386 <307262079@qq.com>
2021-02-28 17:42:57 +0800

    update

---
+++ SaleSystem/CSelectedView.cpp
@@ -0,0 +1,39 @@
+﻿// CSelectedView.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "SaleSystem.h"
+#include "CSelectedView.h"
+
+
+// CSelectedView
+
+IMPLEMENT_DYNCREATE(CSelectedView, CTreeView)
+
+CSelectedView::CSelectedView() {
+
+}
+
+CSelectedView::~CSelectedView() {
+}
+
+BEGIN_MESSAGE_MAP(CSelectedView, CTreeView)
+END_MESSAGE_MAP()
+
+
+// CSelectedView 诊断
+
+#ifdef _DEBUG
+void CSelectedView::AssertValid() const {
+	CTreeView::AssertValid();
+}
+
+#ifndef _WIN32_WCE
+void CSelectedView::Dump(CDumpContext& dc) const {
+	CTreeView::Dump(dc);
+}
+#endif
+#endif //_DEBUG
+
+
+// CSelectedView 消息处理程序
--- SaleSystem/resource.h
+++ SaleSystem/resource.h
@@ -10,6 +10,7 @@
 #define IDI_ICON2                       311
 #define IDI_ICON_RE                     311
 #define IDI_ICON_WIN                    312
+#define IDD_CDisplayView                313
 
 // Next default values for new objects
 // 
@@ -18,6 +19,6 @@
 #define _APS_NEXT_RESOURCE_VALUE        313
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           310
+#define _APS_NEXT_SYMED_VALUE           314
 #endif
 #endif
